30490666c9d7daf1da7cc6b7da442da2ce5e6c29
komaljit <komaljit1874@gmail.com>
2018-05-27 23:54:12 -0700

    minor changes

--- backend/app.js
+++ backend/app.js
@@ -11,13 +11,12 @@ const app = express();
 
 app.use(logger('dev'));
 
-app.use('./public/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('./media', express.static(path.join(__dirname, 'media')));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
 
-// console.log(merchant.router);
 
 app.use('/', merchant.router);
 
